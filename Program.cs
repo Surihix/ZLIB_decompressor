d4111a15bf4e7e498ab91dcfbabda4821286df3e
Surihix <neweramysterymailer@gmail.com>
2023-09-27 18:59:23 +0530

    removed sharpziplib library

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using ICSharpCode.SharpZipLib.Zip.Compression.Streams;
+﻿using Ionic.Zlib;
 using System;
 using System.IO;
 
@@ -8,48 +8,71 @@ namespace ZLIB_decompressor
     {
         static void Main(string[] args)
         {
-            // Check the number of arguements specified by the user 
-            // and if its less than 3, then terminate the program
-            if (args.Length != 3)
+            try
             {
-                Console.WriteLine("Error: Enough arguments not specified");
-                Console.WriteLine("");
-                Console.WriteLine("Specify arguments like this:");
-                Console.WriteLine("ZLIB_decompressor [file.extension] [byte-position] [outputfile.extension]");
-                Console.ReadLine();
-                return;
-            }
-
-            // Storing the values specified in each of the arguements as variables
-            string in_file = args[0];
-            long header_start = Convert.ToInt64(args[1]);
-            string out_file = args[2];
-
-            // Check if the specified in_file in args[0] exists.
-            if (!File.Exists(in_file))
-            {
-                Console.WriteLine("Error: Specified input file does not exist");
-                Console.ReadLine();
-                return;
-            }
-
-            // Open the input file and create the output file specified in
-            // arg[0] and arg[2] in two separate filestreams
-            using (FileStream InFileStream = new FileStream(in_file, FileMode.Open, FileAccess.Read))
-            {
-                using (FileStream OutFileStream = new FileStream(out_file, FileMode.OpenOrCreate))
+                // Check the number of arguements specified by the user 
+                // and if its less than 3, then terminate the program
+                if (args.Length < 3)
                 {
-                    // Seek to the byte value specified in arg[1]
-                    InFileStream.Seek(header_start, SeekOrigin.Begin);
+                    Console.WriteLine("Error: Enough arguments not specified");
+                    Console.WriteLine("");
+                    Console.WriteLine("Specify arguments like this:");
+                    Console.WriteLine("ZLIB_decompressor [file.extension] [byte-position] [outputfile.extension]");
+                    Console.ReadLine();
+                    return;
+                }
 
-                    // Open the input file inside a Inflatter input stream and copy the
-                    // decompressed zlib data into the output file
-                    using (InflaterInputStream ZLib_decmp = new InflaterInputStream(InFileStream))
+                // Storing the values specified in each of the arguements as variables
+                string inFile = args[0];
+                long headerStart = Convert.ToInt64(args[1]);
+                string outFile = args[2];
+
+                // Check if the specified in_file in args[0] exists.
+                if (!File.Exists(inFile))
+                {
+                    Console.WriteLine("Error: Specified input file does not exist");
+                    Console.ReadLine();
+                    return;
+                }
+
+                Console.WriteLine("Decompressing....");
+
+                // Open the input file and create the output file specified in
+                // arg[0] and arg[2] in two separate filestreams
+                using (FileStream inFileStream = new FileStream(inFile, FileMode.Open, FileAccess.Read))
+                {
+                    if (File.Exists(outFile))
                     {
-                        ZLib_decmp.CopyTo(OutFileStream);
+                        File.Delete(outFile);
+                    }
 
+                    using (FileStream outFileStream = new FileStream(outFile, FileMode.OpenOrCreate, FileAccess.Write))
+                    {
+                        // Seek to the specified byte position
+                        // from arg[1]
+                        inFileStream.Seek(headerStart, SeekOrigin.Begin);
+
+                        // Attach outfileStream to decompressor
+                        // and copy the inFileStream into the
+                        // decompressor
+                        // This will decompress the zlib data and 
+                        // the decompressed data will be stored 
+                        // inside the specified outFile
+                        using (ZlibStream decompressor = new ZlibStream(outFileStream, CompressionMode.Decompress))
+                        {
+                            inFileStream.CopyTo(decompressor);
+                        }
                     }
                 }
+
+                Console.WriteLine("Decompressed");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Error: " + ex);
+                Console.ReadLine();
+                return;
             }
         }
     }
